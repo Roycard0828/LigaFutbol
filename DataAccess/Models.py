6c9ecf9520cd5a78316581241c263e76874f934b
Roycard0828 <gamr_0111@outlook.es>
2022-03-15 18:24:06 -0600

    Se corrigio el error en el modelo Equipo

--- DataAccess/Models.py
+++ DataAccess/Models.py
@@ -13,54 +13,10 @@ class Equipo(base):
     representante = Column(String)
     campo = Column(String)
     # Relaciones
-    tablageneral = relationship('TablaGeneral', backref='equipo')
+    # tablageneral = relationship('TablaGeneral', backref='equipo')
 
     def __init__(self, nombre, representante, campo):
         self.nombre = nombre
         self.representante = representante
         self.campo = campo
-
-
-class Partido(base):
-    # ---Atributos de la base de datos---
-    __tablename__ = 'partido'
-    id = Column(Integer, primary_key=True)
-    equipo_local_id = Column(Integer, ForeignKey('equipo.id'))
-    equipo_visit_id = Column(Integer, ForeignKey('equipo.id'))
-    resultado = Column(String, default="Sin resultado")
-    # Relaciones
-    equipo_local = relationship('Equipo', foreign_keys=[equipo_local_id])
-    equipo_visit = relationship('Equipo', foreign_keys=[equipo_visit_id])
-    jornada = relationship('Jornadas', backref='partido')
-
-    def __init__(self, equipo_local, equipo_visit):
-        self.equipo_local = equipo_local
-        self.equipo_visit = equipo_visit
-
-    @property
-    def resultado(self):
-        return self.resultado
-
-    @resultado.setter
-    def resultado(self, resultado: String):
-        self.resultado = resultado
-
-
-class Jornadas(base):
-    # ---Atributos de la base de datos---
-    __tablename__ = 'jornadas'
-    id = Column(Integer, primary_key=True)
-    partido_id = Column(Integer, ForeignKey('partido.id'))
-
-
-class TablaGeneral(base):
-    # ---Atributos de la basde de datos---
-    __tablename__ = 'tablageneral'
-    equipo_id = Column(Integer, ForeignKey('equipo.id'))
-    numero_juegos = Column(Integer, default=0)
-    juegos_ganados = Column(Integer, default=0)
-    juegos_perdidos = Column(Integer, default=0)
-    juegos_empatados = Column(Integer, default=0)
-    numero_goles = Column(Integer, default=0)
-    numero_puntos = Column(Integer, default=0)
 
