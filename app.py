1fcc58ee206c47893bb2954e218bbcf95289e77c
Roycard0828 <gamr_0111@outlook.es>
2022-03-22 18:45:36 -0600

    Se agrega la funcionalidad para actualizar equipos #4

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from Presentacion.ventana_principal import *
-from Controller import controlador
+from Presentacion.ventana_actualizar import *
+from Controller.controlador import EquiposController
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -12,14 +13,35 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.BtnTabla.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.PaginaTabla))
 
         # ------ Metodos de equipos --------
-        self.BtnInsertar.clicked.connect(lambda: self.datos_equipos())
+        self.BtnInsertar.clicked.connect(lambda: self.insertar_datos_equipos())
+        self.BtnActualizar.clicked.connect(lambda: self.actualizar_datos_equipos())
 
-    def datos_equipos(self):
+    def insertar_datos_equipos(self):
         nombre = str(self.TxtNombreEquipo.toPlainText())
         representante = str(self.TxtRepresentanteEquipo.toPlainText())
         campo = str(self.TxtCampoEquipo.toPlainText())
 
-        controlador.insertar_equipos(nombre, representante, campo)
+        EquiposController.insertar_equipos(nombre, representante, campo)
+
+    def borrar_datos_equipos(self):
+        id = 0  # Recopilar el id de la tabla
+        EquiposController.borrar_equipos(id)
+
+    def actualizar_datos_equipos(self):
+        id = 31  # Dato recopilado de la tabla
+        self.widget = QtWidgets.QMainWindow()
+        self.actualizar_widget = Ui_ActualizarWIndow()
+        self.actualizar_widget.setupUi(self.widget)
+        self.widget.show()
+
+        def definir_datos():
+            nombre = str(self.actualizar_widget.TxtNombre.toPlainText())
+            representante = str(self.actualizar_widget.TxtRepresentante.toPlainText())
+            campo = str(self.actualizar_widget.TxtCampo.toPlainText())
+
+            EquiposController.actualizar_equipos(id, nombre, representante, campo)
+
+        self.actualizar_widget.BtnAceptar.clicked.connect(lambda: definir_datos())
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -9,8 +9,8 @@ def run():
     # real = Equipo("Atletico de Madrid", "XXXXXX", "Wanda")
     dao_equipo = DaoEquipoFactory().create_entity()
     # dao_equipo.crear(real)
-    # equipo_des = dao_equipo.leer(3)
-    # dao_equipo.actualizar(equipo_des, "Atletico", "Cholo", "Wanda")
+    equipo_des = dao_equipo.leer(31)
+    dao_equipo.actualizar(equipo_des, "RealMadrid", "Ancelotti", "Bernabeu")
     # dao_equipo.borrar(equipo_des)
     # lista = dao_equipo.leer_todos()
 
