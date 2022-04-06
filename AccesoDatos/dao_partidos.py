46250de76c99644e6bf2a1f12e753a76a322c639
Roycard0828 <gamr_0111@outlook.es>
2022-04-06 08:11:58 -0500

    Se agrega el modelo del partido y corrigen errores #10

---
+++ AccesoDatos/dao_partidos.py
@@ -0,0 +1,27 @@
+from .modelos import Partido
+from .dao_general import DaoGeneral
+from .db import session
+
+
+class DaoPartido(DaoGeneral):
+
+    def __init__(self):
+        self.session = session
+
+    def guardar(self, partido: Partido):
+        self.session.add(partido)
+        self.session.commit()
+        return True
+
+    def leer(self, id: int):
+        consulta = self.session.query(Partido).get(id)
+        return consulta
+
+    def leer_todos(self):
+        pass
+
+    def actualizar(self, id: int, *args):
+        pass
+
+    def borrar(self, partido: Partido):
+        pass
